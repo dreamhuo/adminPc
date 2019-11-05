1df3d0eba796e46dd50124e93b8b6b1fb6598797
霍小生 <huocs>
2019-11-05 20:12:47 +0800

    更新配置文件

--- src/config/index.js
+++ src/config/index.js
@@ -18,34 +18,27 @@ switch (process.env.VUE_APP_MODE) {
     break
 }
 export default {
-  /**
-   * @description 配置显示在浏览器标签的title
-   */
+  // 配置显示在浏览器标签的title
   title: 'admin',
-  /**
-   * @description token在Cookie中存储的天数，默认1天
-   */
+  // token在Cookie中存储的天数，默认1天
   cookieExpires: 1,
   /**
    * @description 是否使用国际化，默认为false
    *              如果不使用，则需要在路由中给需要在菜单中展示的路由设置meta: {title: 'xxx'}
    *              用来在菜单中显示文字
    */
-  useI18n: true,
+  useI18n: false,
   /**
-   * @description 应用业务各环境差异
+   * 应用业务各环境差异
+   * 常量会配置在 dev.js、prod.js、test.js、uat.js 里
    */
   appConf: appConf,
-  /**
-   * @description 默认打开的首页的路由name值，默认为home
-   */
+  // 默认打开的首页的路由name值，默认为home
   homeName: 'home',
-  /**
-   * @description 需要加载的插件
-   */
+  // 需要加载的插件
   plugin: {
     'error-store': {
-      showInHeader: true, // 设为false后不会在顶部显示错误日志徽标
+      showInHeader: false, // 设为false后不会在顶部显示错误日志徽标
       developmentOff: true // 设为true后在开发环境不会收集错误信息，方便开发中排查错误
     }
   }
